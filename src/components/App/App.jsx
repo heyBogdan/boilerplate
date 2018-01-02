91d9a3ab3ba8064ce67d92abc86695f167a91935
heyBogdan <bokhonovb@gmail.com>
2018-01-02 21:29:19 +0300

    Remove app implementation, add independent components

--- src/components/App/App.jsx
+++ src/components/App/App.jsx
@@ -1,29 +1,12 @@
 import React from "react";
-import MuiThemeProvider from "material-ui/styles/MuiThemeProvider";
-import { List, ListItem } from "material-ui/List";
-import ContentInbox from "material-ui/svg-icons/content/inbox";
-import ActionGrade from "material-ui/svg-icons/action/grade";
-import ContentSend from "material-ui/svg-icons/content/send";
-import ContentDrafts from "material-ui/svg-icons/content/drafts";
 
 import { Switch, Route } from "react-router-dom";
-import Arm from "../Arm/Arm";
+import Page from "../Page/Page";
 
 const App = () => (
-    <MuiThemeProvider>
-        <div className="sidebar">
-            <List className="sidebar_content">
-                <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-                <ListItem primaryText="Starred" leftIcon={<ActionGrade />} />
-                <ListItem primaryText="Sent mail" leftIcon={<ContentSend />} />
-                <ListItem primaryText="Drafts" leftIcon={<ContentDrafts />} />
-                <ListItem primaryText="Inbox" leftIcon={<ContentInbox />} />
-            </List>
-        </div>
-        <Switch>
-            <Route path="/orders" component={Arm} />
-        </Switch>
-    </MuiThemeProvider>
+    <Switch>
+        <Route path="/" component={Page} />
+    </Switch>
 );
 
 export default App;
---
+++ src/components/Page/Page.jsx
@@ -0,0 +1,5 @@
+import React from "react";
+
+const Page = () => <div>Content</div>;
+
+export default Page;
